60be900ce0dda3e37be8afe8ff2dca967fe5a7aa
Sukalyan <sadhikary0055@gmail.com>
2023-01-15 22:38:14 +0530

    added thank you

--- form-making.py
+++ form-making.py
@@ -15,6 +15,7 @@ def getvals():
 
 
 root.geometry("644x344")
+
 #Heading
 Label(root, text="Welcome to Sukalyan's Travels", font="comicsansms 13 bold", pady=15).grid(row=0, column=3)
 
@@ -24,6 +25,7 @@ phone = Label(root, text="Phone")
 gender = Label(root, text="Gender")
 emergency = Label(root, text="Emergency Contact")
 paymentmode = Label(root, text="Payment Mode")
+year=Label(root,text="year")
 
 #Pack text for our form
 name.grid(row=1, column=2)
@@ -31,6 +33,7 @@ phone.grid(row=2, column=2)
 gender.grid(row=3, column=2)
 emergency.grid(row=4, column=2)
 paymentmode.grid(row=5, column=2)
+year.grid(row=6,column=2)
 
 # Tkinter variable for storing entries
 namevalue = StringVar()
@@ -38,6 +41,7 @@ phonevalue = StringVar()
 gendervalue = StringVar()
 emergencyvalue = StringVar()
 paymentmodevalue = StringVar()
+yearvalue=StringVar()
 foodservicevalue = IntVar()
 
 
@@ -47,6 +51,7 @@ phoneentry = Entry(root, textvariable=phonevalue)
 genderentry = Entry(root, textvariable=gendervalue)
 emergencyentry = Entry(root, textvariable=emergencyvalue)
 paymentmodeentry = Entry(root, textvariable=paymentmodevalue)
+yearentry=Entry(root,textvariable="yearvalue")
 
 # Packing the Entries
 nameentry.grid(row=1, column=3)
@@ -54,13 +59,14 @@ phoneentry.grid(row=2, column=3)
 genderentry.grid(row=3, column=3)
 emergencyentry.grid(row=4, column=3)
 paymentmodeentry.grid(row=5, column=3)
+yearentry.grid(row=6,column=3)
 
 #Checkbox & Packing it
 foodservice = Checkbutton(text="Want to prebook your meals?", variable = foodservicevalue)
-foodservice.grid(row=6, column=3)
+foodservice.grid(row=8, column=3)
 
 #Button & packing it and assigning it a command
-Button(text="Submit", command=getvals,bg="red",font="comicsans 12 bold").grid(row=7, column=3)
+Button(text="Submit", command=getvals,bg="green",font="comicsans 12 bold").grid(row=11, column=3)
 
 
 
